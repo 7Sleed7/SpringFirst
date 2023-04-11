ad10f3b58e30674c3228db7e47ab4116817787ae
Sleed <100526537+7Sleed7@users.noreply.github.com>
2023-04-11 22:40:03 +0300

    second and third task

---
+++ src/main/java/ru/sleed/secondtask/controller/NameController.java
@@ -0,0 +1,24 @@
+package ru.sleed.secondtask.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.sleed.secondtask.service.impl.ServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("name/")
+public class NameController {
+    private final ServiceImpl serviceImpl;
+
+    @Autowired
+    public NameController(ServiceImpl serviceImpl) {
+        this.serviceImpl = serviceImpl;
+    }
+
+    @GetMapping("getName")
+    public String getString(@RequestParam("name") String name) {
+        return serviceImpl.nameCreation(name);
+    }
+}
