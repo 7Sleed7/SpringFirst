6ddd2300025fb9c1ad65d968a7672a43b5389ced
Sleed <100526537+7Sleed7@users.noreply.github.com>
2023-04-18 10:38:02 +0300

    ExceptionHandler

--- thridTask/src/main/java/ru/sleed/thridtask/controller/advice/ControllerAdvice.java
+++ thridTask/src/main/java/ru/sleed/thridtask/controller/advice/ControllerAdvice.java
@@ -3,18 +3,18 @@ package ru.sleed.thridtask.controller.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 import ru.sleed.thridtask.exeption.ApplicationException;
 import ru.sleed.thridtask.model.dto.ExceptionDto;
 
 @Slf4j
-@RestControllerAdvice
+@RestController
 public class ControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<String> handler(ApplicationException applicationException) {
-        ExceptionDto exceptionDto = ExceptionDto.exceptionToDto(applicationException);
-        log.error(exceptionDto.getExceptionMessage(), exceptionDto);
-        return new ResponseEntity<>(exceptionDto.getExceptionMessage(), exceptionDto.getStatus());
+        ExceptionDto exception = ExceptionDto.exceptionToDto(applicationException);
+        log.error(exception.getExceptionMessage(), exception);
+        return new ResponseEntity<>(exception.getExceptionMessage(), exception.getStatus());
     }
 }
